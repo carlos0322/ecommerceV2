9bdea80e04a4f2409d0925885963c5765a805227
carlos0322 <carlos.dc.lopez@gmail.com>
2023-01-08 18:39:21 -0500

    test

--- database.js
+++ database.js
@@ -40,7 +40,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/Products1", (req, res) => {
+app.get("/Products", (req, res) => {
   const sql = "SELECT * FROM products";
   pool.query(sql, (err, results) => {
     res.send(results);
